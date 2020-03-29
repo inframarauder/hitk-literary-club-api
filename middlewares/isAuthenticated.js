82e78f1b178b928edc9d2c371c520710c4d27c2c
Subhasis Das <subhasisdas125@gmail.com>
2020-03-30 01:41:23 +0530

    added authentication middleware

---
+++ middlewares/isAuthenticated.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  let authToken = req.header("x-auth-token");
+  if (!authToken) {
+    return res.status(403).json({ error: "Access denied,token missing!" });
+  } else {
+    try {
+      const payload = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+      req.user = payload;
+      next();
+    } catch (err) {
+      logger.error(err.message, { meta: err });
+      return res.status(400).json({ error: err.message });
+    }
+  }
+};
